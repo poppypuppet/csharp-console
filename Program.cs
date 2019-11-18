ef5f3a0c6b91fc9e236320ad755ac50054ee84a6
Victoria Zhang <weiwzhan@microsoft.com>
2019-11-18 13:26:52 -0800

    stagde

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Azure.Devices.Client;
@@ -8,25 +9,18 @@ using Newtonsoft.Json;
 
 namespace console
 {
-    class LogLine
-    {
-        [JsonProperty]
-        public string UTC { get; set; }
-        [JsonProperty]
-        public string LocalTime { get; set; }
-        [JsonProperty]
-        public string LogLevel { get; set; }
-        [JsonProperty]
-        public string Pid { get; set; }
-        [JsonProperty]
-        public string Class { get; set; }
-        [JsonProperty]
-        public string Data { get; set; }
-
-
-    }
     class Program
     {
+
+        // dd/MMM/yyyy:HH:mm:ss zzz
+        // 2019-10-30T23:31:19.915433969Z 2019-10-30 23:31:19:915 - [verbose] - 6484 ArchonAG-graph1.ex1_a  - SingleEdgeIoGatherer::handleIncomingMessage  [handleIncomingMessage()][/go/src/RaaS.PerceptionEngine.Core/libArchon/archonag/./inputGatherer/singleEdgeIoGatherer.h:25] 
+        // 2019-10-30T23:31:19.915433969Z 2019-10-30 23:31:19:915 [verbose] 6484 <ArchonAG-graph1.ex1_a> SingleEdgeIoGatherer::handleIncomingMessage  [handleIncomingMessage()][/go/src/RaaS.PerceptionEngine.Core/libArchon/archonag/./inputGatherer/singleEdgeIoGatherer.h:25] 
+        // ^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z) (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{3}) - [(\a+)] - (\d+) .+
+        public static Regex regex2 = new Regex(@"(?<UTC>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z)(?<DATA>.+)");
+        public static Regex regex = new Regex(@"(?<UTC>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z) (?<LocalTime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{3}) - \[(?<LOGLEVEL>\w+)\] - (?<PID>\d+) (?<CLASS>.+) - (?<DATA>.+)");
+
+        public static char[] charsToTrim = { ' ', '\n', '\r' };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -41,64 +35,164 @@ namespace console
             Console.WriteLine(csb.SharedAccessKeyName);
             Console.WriteLine(csb.SharedAccessSignature);
 
-            //string file = @"C:\Users\weiwzhan\Desktop\logs.txt";
+            Console.WriteLine(DateTime.Now.AddHours(-1).ToUniversalTime().ToString());
+            Console.WriteLine(DateTimeOffset.Now);
+
+            DateTimeOffset dtf = DateTimeOffset.FromUnixTimeSeconds(1572917090);
+            Console.WriteLine(dtf.UtcDateTime);
+            Console.WriteLine(DateTime.Parse("2019-10-30T23:31:19.915433969Z"));
+
+
+        }
+
+        public static void logs()
+        {
+            var meta = new Dictionary<String, String>();
+            meta.Add("ContainerId", "container.ID");
+            meta.Add("ContainerImage", "container.Image");
+            meta.Add("ContainerState", "container.State");
+            var logsObj = new List<Dictionary<string, string>>();
+            LogsMeta logsMeta = null;
+            (logsMeta, logsObj) = PharseStream(meta);
+            Console.WriteLine(JsonConvert.SerializeObject(logsObj));
+            Console.WriteLine(logsObj.Count);
+
+            ThrottlePost(logsObj);
+
+            // long[] timefilter = { 1572478279916, 1572478279917 };
+            // Dictionary<LogFlag, string> valueFilters = new Dictionary<LogFlag, string>();
+            // valueFilters.Add(LogFlag.CLASS, "NODE_EX1");
+            // var res = FilterStream(logsObj, timefilter, valueFilters);
+            // //Console.WriteLine(JsonConvert.SerializeObject(res));
+            // Console.WriteLine(res.Count);
+
+            string payloads = "{\"TS\":-1,\"TE\":-1,\"ID\":null,\"Interval\":1000,\"Filters\":{}}";
+            var requestPayloadAsJson = JsonConvert.DeserializeObject<DirectMethodPayloads>(payloads);
+            Console.WriteLine("Receive request payloads = {0}", JsonConvert.SerializeObject(requestPayloadAsJson));
+
+            IList<LogFlag> flags = new List<LogFlag> { LogFlag.UTC, LogFlag.PID, LogFlag.LOGLEVEL, LogFlag.LOCALTIME, LogFlag.DATA, LogFlag.CLASS };
+
+            Dictionary<LogFlag, string> valueFilters = new Dictionary<LogFlag, string>();
+            Dictionary<LogFlag, string> ans = flags.Where(f => requestPayloadAsJson.Filters.ContainsKey(f.Value))
+            .ToDictionary(f => f, f => requestPayloadAsJson.Filters[f.Value]);
+            Console.WriteLine(JsonConvert.SerializeObject(ans));
+
+        }
+
+        public static void ThrottlePost(List<Dictionary<String, String>> payloads, int throttle = 10)
+        {
+            int length = payloads.Count;
+            List<Object> temp = new List<Object>();
+            for (int i = 0; i < length; i = i + throttle)
+            {
+                var items = payloads.Skip(i).Take(throttle);
+                Console.WriteLine(JsonConvert.SerializeObject(items));
+            }
+        }
+
+        public static (LogsMeta, List<Dictionary<String, String>>) PharseStream(Dictionary<String, String> meta)
+        {
             //string archon = @"archon.log";
-            string node = @"node.log";
+            string node = "node.log";
             //string replacement = Regex.Replace(s, @"\t|\n|\r", "");
-            int linecounter = 0;
-            int matchcounter = 0;
-            string line;
+            int lineCounter = 0;
+            int matchCounter = 0;
+            var logsObj = new List<Dictionary<string, string>>();
 
-
-
-            // dd/MMM/yyyy:HH:mm:ss zzz
-            // 2019-10-30T23:31:19.915433969Z 2019-10-30 23:31:19:915 - [verbose] - 6484 ArchonAG-graph1.ex1_a  - SingleEdgeIoGatherer::handleIncomingMessage  [handleIncomingMessage()][/go/src/RaaS.PerceptionEngine.Core/libArchon/archonag/./inputGatherer/singleEdgeIoGatherer.h:25] 
-            // 2019-10-30T23:31:19.915433969Z 2019-10-30 23:31:19:915 [verbose] 6484 <ArchonAG-graph1.ex1_a> SingleEdgeIoGatherer::handleIncomingMessage  [handleIncomingMessage()][/go/src/RaaS.PerceptionEngine.Core/libArchon/archonag/./inputGatherer/singleEdgeIoGatherer.h:25] 
-            // ^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z) (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{3}) - [(\a+)] - (\d+) .+
-
-            Regex regex2 = new Regex(@"^(?<UTC>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z)(?<DATA>.+)");
-            Regex regex = new Regex(@"^(?<UTC>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z) (?<LocalTime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{3}) - \[(?<LOGLEVEL>\w+)\] - (?<PID>\d+) (?<CLASS>.+) - (?<DATA>.+)");
-            // Read the file and display it line by line.  
-            var logobj = new List<Dictionary<string, string>>();
-            char[] charsToTrim = { ' ', '\n', '\r' };
             using (System.IO.StreamReader sr = new System.IO.StreamReader(node))
             {
+                string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    var dic = new Dictionary<string, string>();
+                    var dic = new Dictionary<String, String>();
                     var maches = regex.Matches(line);
                     if (regex.IsMatch(line))
                     {
+                        // match full logs
                         maches = regex.Matches(line);
                     }
                     else if (regex2.IsMatch(line))
                     {
+                        // match logs only with timestamp
                         maches = regex2.Matches(line);
                     }
 
                     foreach (Match match in maches)
                     {
-                        Console.WriteLine(match.Groups.Count);
                         foreach (string name in regex.GetGroupNames())
                         {
+                            // "0" means whole string
                             if (name != "0")
                             {
                                 //dic[name] = Regex.Replace(match.Groups[name].Value, @"\t|\n|\r", "");
                                 dic[name] = match.Groups[name].Value.Trim(charsToTrim);
                             }
-                            Console.WriteLine("{0}:{1}", name, match.Groups[name]);
                         }
-                        matchcounter++;
+                        matchCounter++;
                     }
-                    logobj.Add(dic);
-                    linecounter++;
+                    if (dic.Any())
+                    {
+                        foreach (var p in meta)
+                        {
+                            dic.Add(p.Key, p.Value);
+                        }
+                        logsObj.Add(dic);
+                    }
+                    lineCounter++;
                 }
             }
-            Console.WriteLine(JsonConvert.SerializeObject(logobj));
-            // end
-            System.Console.WriteLine("There were {0} lines, {1} lines matched.", linecounter, matchcounter);
-            System.Console.ReadLine();
+            //Console.WriteLine(JsonConvert.SerializeObject(logsObj));
+            return (new LogsMeta { ContainerId = meta["ContainerId"], LineCounter = lineCounter, MatchCounter = matchCounter }, logsObj);
+        }
+
+        public static List<Dictionary<String, String>> FilterStream(List<Dictionary<String, String>> data, long[] timefilter, Dictionary<LogFlag, string> valueFilters)
+        {
+            return data.Where(e =>
+                 {
+                     string ts = e[LogFlag.UTC.Value];
+                     DateTime UnixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+                     long uts = (long)(DateTime.Parse(ts).ToUniversalTime() - UnixStart).TotalMilliseconds;
+                     bool j = uts > timefilter[0] && uts <= timefilter[1];
+                     return uts > timefilter[0] && uts <= timefilter[1];
+                 })
+                 .Where(e =>
+                     valueFilters.All(p => e.ContainsKey(p.Key.Value) && p.Value == e[p.Key.Value])
+                 ).ToList();
+        }
+
+        public class DirectMethodPayloads
+        {
+            public long TS { get; set; }
+
+            public long TE { get; set; }
+
+            public string ID { get; set; }
+
+            public Dictionary<String, String> Filters { get; set; }
+        }
+
+        public class LogsMeta
+        {
+            public string ContainerId { get; set; }
+            public long LineCounter { get; set; }
+            public long MatchCounter { get; set; }
+            public long FetchLogsDuration { get; set; }
+            public long PhaseLogsDuration { get; set; }
+            public long PostLogsDuration { get; set; }
+        }
+
+        public class LogFlag
+        {
+            private LogFlag(string value) { Value = value; }
+
+            public string Value { get; set; }
+
+            public static LogFlag UTC { get { return new LogFlag("UTC"); } }
+            public static LogFlag LOCALTIME { get { return new LogFlag("LOCALTIME"); } }
+            public static LogFlag LOGLEVEL { get { return new LogFlag("LOGLEVEL"); } }
+            public static LogFlag PID { get { return new LogFlag("PID"); } }
+            public static LogFlag CLASS { get { return new LogFlag("CLASS"); } }
+            public static LogFlag DATA { get { return new LogFlag("DATA"); } }
         }
     }
 }
-
