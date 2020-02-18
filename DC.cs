6480ad97458aa663d615ba706242a86111b04d67
weiwzhan <weiwzhan@microsoft.com>
2020-02-18 13:40:30 -0800

    draft

--- DC.cs
+++ DC.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Net.Http;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Net.Http.Client;
 
 namespace PEngineModule.Logs
 {
@@ -11,29 +11,29 @@ namespace PEngineModule.Logs
     {
         public static async Task DockerQuery()
         {
-            const string LIST_REQUEST =
-            "GET /v1.40/containers/json HTTP/1.1\r\n" +
-            "Host: localhost\r\n" +
-            "Accept: */*\r\n" +
-            "\r\n";
+            // const string LIST_REQUEST =
+            // "GET /v1.40/containers/json HTTP/1.1\r\n" +
+            // "Host: localhost\r\n" +
+            // "Accept: */*\r\n" +
+            // "\r\n";
 
-            const string LOG_REQUEST =
-            "GET /v1.40/containers/acd30b303fa6c7b8ec47e06fc36479b1513eeb3a13f75295b85b399a4e435354/logs?timestamps=true&stdout=true&stderr=true HTTP/1.1\r\n" +
-            "Host: localhost\r\n" +
-            "Accept: */*\r\n" +
-            "\r\n";
+            // const string LOG_REQUEST =
+            // "GET /v1.40/containers/acd30b303fa6c7b8ec47e06fc36479b1513eeb3a13f75295b85b399a4e435354/logs?timestamps=true&stdout=true&stderr=true HTTP/1.1\r\n" +
+            // "Host: localhost\r\n" +
+            // "Accept: */*\r\n" +
+            // "\r\n";
 
-            var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
-            socket.ReceiveTimeout = 60 * 1000; // 60 seconds
-            var endpoint = new UnixDomainSocketEndPoint("/var/run/docker.sock");
-            socket.Connect(endpoint);
-            byte[] requestBytes = Encoding.UTF8.GetBytes(LOG_REQUEST);
-            socket.Send(requestBytes);
-            byte[] recvBytes = new byte[socket.ReceiveBufferSize];
-            await socket.SendAsync(requestBytes, SocketFlags.None);
-            int numBytes = socket.Receive(recvBytes, socket.ReceiveBufferSize, SocketFlags.None);
-            socket.Disconnect(false);
-            Console.WriteLine(Encoding.UTF8.GetString(recvBytes));
+            // var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
+            // socket.ReceiveTimeout = 60 * 1000; // 60 seconds
+            // var endpoint = new UnixDomainSocketEndPoint("/var/run/docker.sock");
+            // socket.Connect(endpoint);
+            // byte[] requestBytes = Encoding.UTF8.GetBytes(LOG_REQUEST);
+            // socket.Send(requestBytes);
+            // byte[] recvBytes = new byte[socket.ReceiveBufferSize];
+            // await socket.SendAsync(requestBytes, SocketFlags.None);
+            // int numBytes = socket.Receive(recvBytes, socket.ReceiveBufferSize, SocketFlags.None);
+            // socket.Disconnect(false);
+            // Console.WriteLine(Encoding.UTF8.GetString(recvBytes));
 
             // using (var resultStream = new MemoryStream())
             // {
@@ -53,6 +53,34 @@ namespace PEngineModule.Logs
             //     Console.WriteLine("done");
             //     Console.WriteLine(resultStream.ToString());
             // }
+
+            // SocketsHttpHandler handler = new SocketsHttpHandler();
+            // HttpClient client = new HttpClient(handler);
+            // client.Timeout = TimeSpan.FromSeconds(60);
+
+
+            var uri = new Uri("unix:///var/run/docker.sock");
+            if (uri.Scheme.ToLowerInvariant() != "unix")
+            {
+                return;
+            }
+
+            var pipeString = uri.LocalPath;
+            var handler = new ManagedHandler(async (string host, int port, CancellationToken cancellationToken) =>
+            {
+                var sock = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+                await sock.ConnectAsync(new UnixDomainSocketEndPoint("/var/run/docker.sock"));
+                return sock;
+            });
+
+            var client = new HttpClient(handler);
+            CancellationToken cancellationToken = new CancellationToken();
+            var request = new HttpRequestMessage(HttpMethod.Get, "/v1.40/containers/json");
+            request.Headers.Add("Host", "localhost");
+            request.Headers.Add("Accept", "*/*");
+            Console.WriteLine(request.ToString());
+            var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            Console.WriteLine(response.ToString());
         }
     }
 }
